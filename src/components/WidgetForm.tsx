8293adbd6056d57ccb441d712d3970ea3ec9a42f
Wesley Maciel <weslmaciel@gmail.com>
2022-05-07 23:07:34 -0300

    global styles

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,7 @@
+export const WidgetForm = () => { 
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg">
+            hello word
+        </div>
+    )
+}
