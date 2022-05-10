4ec09132934826f99a32c92acd6acdc3d597cf77
Wesley Maciel <weslmaciel@gmail.com>
2022-05-10 01:31:50 -0300

    prettier format code of project

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,6 +1,6 @@
 import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from "@headlessui/react";
-import { WidgetForm } from "./WidgetForm"
+import { WidgetForm } from "./WidgetForm";
 
 export const Widget = () => {
   return (
