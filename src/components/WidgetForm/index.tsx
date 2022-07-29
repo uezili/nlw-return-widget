905c33d1f20f24ac497af93aaf23b0eb5c63aa3b
Wesley Maciel <weslmaciel@gmail.com>
2022-07-28 21:49:20 -0300

    Start feedbacks components

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { CloseButton } from "../CloseButton";
+import { FeedbackTypesSteps } from "./Steps/FeedbackTypeStep";
 
-import bugImageUrl from "../../assets/bug.svg";
-import ideaImageUrl from "../../assets/idea.svg";
-import thoughtImageUrl from "../../assets/thought.svg";
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problem",
     image: {
@@ -29,9 +30,9 @@ const feedbackTypes = {
   },
 };
 
-type FeedbackType = keyof typeof feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes
 
-export const WidgetForm = () => {
+export function WidgetForm() {
 
   const [ feedbackType, setFeedbackType ] = useState<FeedbackType | null>(null)
 
@@ -43,26 +44,12 @@ export const WidgetForm = () => {
         <CloseButton />
       </header>
       {!feedbackType ? (
-          <div className="flex py-8 gap-2 w-full">
-          { Object.entries(feedbackTypes).map(([key, value]) => {
-            return (
-              <button
-              key={key}
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              onClick={() => setFeedbackType(key as FeedbackType)}
-              type="button"
-              >
-                <img src={value.image.source} alt={value.title} />
-                <span>{value.title}</span>
-              </button>
-            )
-          }) }
-        </div>
+         <FeedbackTypesSteps onFeedbackTypeChanged={setFeedbackType} />
       ) : (
         <p>Hellow Word</p>
       )}
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela{" "}
+        Feito com ♥ pela {""}
         <a
           className="underline underline-offset-2"
           href="http://rocketseat.com.br"
