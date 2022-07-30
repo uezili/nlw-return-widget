d46ccb137ecd8ff49ac1402708c4db59a1f5abe9
Wesley Maciel <weslmaciel@gmail.com>
2022-07-30 16:10:14 -0300

    add path FeedbackContentStep

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import { FeedbackTypesSteps } from "./Steps/FeedbackTypeStep";
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -39,14 +40,11 @@ export function WidgetForm() {
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
+
       {!feedbackType ? (
          <FeedbackTypesSteps onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hellow Word</p>
+        <FeedbackContentStep feedbackType={feedbackType}/>
       )}
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela {""}
